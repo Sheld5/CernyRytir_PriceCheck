7fd3a6c7f7e3968e01af4a76068ac8b92871f61d
Sheld5 <simonsoldat@seznam.cz>
2019-07-11 00:16:56 +0200

    implemented parsing deck lists with cards and their quantities and calculating the price total

--- src/com/github/sheld5/CernyRytir_PriceCheck/main/Main.java
+++ src/com/github/sheld5/CernyRytir_PriceCheck/main/Main.java
@@ -5,21 +5,52 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.*;
+import java.util.regex.Pattern;
 
 public class Main {
 
     private static final String DEFAULT_URL_1 = "http://cernyrytir.cz/index.php3?akce=3&limit=";
     private static final String DEFAULT_URL_2 = "&searchtype=card&searchname=";
     private static final String DEFAULT_URL_3 = "&hledej_pouze_magic=1&submit=Vyhledej";
+    private static final String QUANTITY_REGEX = "^[0-9]+[x]?$";
 
     public static void main(String[] args) {
+        
 
-        try {
-            System.out.println(getCardPrice("Forest"));
-        } catch (IOException e) {
-            e.printStackTrace();
+    }
+
+    private static int getTotalPrice(String cardList) {
+        // Create String[] of all cards and int[] of their quantities
+        Pattern pattern = Pattern.compile(QUANTITY_REGEX);
+        String[] list = cardList.split("\n");
+        int[] quantities = new int[list.length];
+        for (int i = 0; i < list.length; i++) {
+            String[] words = list[i].split(" ");
+            if (pattern.matcher(words[0]).matches()) {
+                quantities[i] = Integer.parseInt(words[0].replace("x", ""));
+                list[i] = "";
+                for (int o = 1; o < words.length; o++) {
+                    if (o == 1) {
+                        list[i] += words[o];
+                    } else {
+                        list[i] += " " + words[o];
+                    }
+                }
+            } else {
+                quantities[i] = 1;
+            }
         }
 
+        int priceTotal = 0;
+        for (int i = 0; i < list.length; i++) {
+            try {
+                priceTotal += quantities[i] * getCardPrice(list[i]);
+            } catch (IOException e) {
+                System.out.println("ERROR: Could not find price for card \"" + list[i] + "\"");
+                System.out.println("The card has been excluded from the total calculated price.");
+            }
+        }
+        return priceTotal;
     }
 
     // Finds the last page of the printings of the card and returns the last price on that page. (That is the lowest price on "cernyrytir.cz".)
